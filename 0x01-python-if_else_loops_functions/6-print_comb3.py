ccf4cb9b3f3d655a4099f9a793944858821d0d9b
Victor Okpe <okpevictor84@gmail.com>
2023-10-03 16:26:07 -0700

    ggpapa

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for digit1 in range(10):
-    for digit2 in range(digit1 + 1, 10):
-        print("{:02d}, {:02d}".format(digit1, digit2), end=", " if digit2 < 9 else "\n")
+
+for a in range(0, 10):
+    for b in range(a, 10):
+        if a != b:
+            if a == 8 and b == 9:
+                print("{0}{1}".format(a, b))
+            else:
+                print("{0}{1}, ".format(a, b), end='')
