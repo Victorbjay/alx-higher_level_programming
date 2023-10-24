82a67b4e51a0ade58d8627004a5921d74aeba5fd
Victor Okpe <okpevictor84@gmail.com>
2023-10-24 12:46:48 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Empty Square Script.
+
+This script defines a Square.
+"""
+
+
+class Square():
+    """Square Class.
+
+    This class defines and empty square.
+    """
+
+    pass
